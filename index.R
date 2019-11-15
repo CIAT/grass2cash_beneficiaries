b2e2feac69386fe902da419dc2864dbc9350984c
John Mutua <jyumbya@yahoo.com>
2019-11-15 19:19:48 +0300

    added eth data

--- index.R
+++ index.R
@@ -15,6 +15,7 @@ library(leaflet.extras)
 iDir <- "."
 
 wards  <- readOGR(paste0(iDir, "/data/ward_boundaries.shp"))
+woredas  <- readOGR(paste0(iDir, "/data/woreda_boundaries.shp"))
 ben_wards <- read_csv(paste0(iDir, "/data/", "benefeciaries.csv"))
 
 # #spatial join
@@ -22,7 +23,7 @@ ben_wards <- merge(wards, ben_wards, by.x = "ward", by.y = "ward_name", duplicat
 
 ben_wards_data <- read_csv(paste0(iDir, "/data/", "benefeciaries_collapsed.csv"))
 
-ben_wards_data$total_beneficiaries <- apply(ben_wards_data[,9:15], 1, sum, na.rm=TRUE)
+ben_wards_data$total_beneficiaries <- apply(ben_wards_data[,4:16], 1, sum, na.rm=TRUE)
 
 demo_trials <- read.csv(paste0(iDir, "/data/", "demo_trials.csv"), stringsAsFactors=FALSE)
 
@@ -30,11 +31,12 @@ demo_trials <- read.csv(paste0(iDir, "/data/", "demo_trials.csv"), stringsAsFact
 
 #labels
 ward_names <- paste("Ward Name: ", ben_wards@data$ward)
+woreda_names <- paste("Woreda Name: ", woredas@data$admin_name)
 popup1 <- paste0("Trial Name: ", demo_trials$Trial_Name)
 
-#colors <- c("#B2182B", "#D6604D", "#F4A582", "#FDDBC7", "#D1E5F0", "#92C5DE", "#4393C3" )
+# colors <- c("red", "green", "yellow", "violet", "blue", "khaki", "cyan" )
 
-colors <- c("red", "green", "yellow", "violet", "blue", "khaki", "cyan" )
+colors <- c("#6A3D9A", "#FB9A99", "#BEAED4", "#B3E2CD", "#F4CAE4", "#FFF2AE", "#B3CDE3", "#984EA3", "#DECBE4", "#FB8072", "#F1E2CC", "#666666", "#A65628")
 
 map.ll <- leaflet() %>%
 
@@ -55,17 +57,26 @@ map.ll <- leaflet() %>%
               dashArray = "3",
               fillOpacity = 0,
               group = "Ward boundaries",
-              # highlight = highlightOptions(weight = 5,
-              #                              color = "white",
-              #                              dashArray = "",
-              #                              bringToFront = TRUE),
               label = ward_names,
               labelOptions = labelOptions(style = list(`font-weight` = "normal",
                                                        padding = "3px 8px"), textsize = "15px", direction = "auto")) %>%
+  
+  addPolygons(data = woredas,
+              weight = 2,
+              color = "brown" ,
+              dashArray = "3",
+              fillOpacity = 0,
+              group = "Woreda boundaries",
+              label = woreda_names,
+              labelOptions = labelOptions(style = list(`font-weight` = "normal",
+                                                       padding = "3px 8px"), textsize = "15px", direction = "auto")) %>%
+  
 
   addMinicharts(ben_wards_data$POINT_X, ben_wards_data$POINT_Y,
                 type = "pie",
-                chartdata = ben_wards_data[, c("Brachiaria Cayman", "Brachiaria MG4", "Brachiaria Mulato II", "Brachiaria Xaraes", "Panicum Masaai", "Panicum Mombasa", "Panicum Tanzania")],
+                chartdata = ben_wards_data[, c("Brachiaria cayman", "Brachiaria MG4", "Brachiaria mulato II", "Brachiaria xaraes", 
+                                               "Panicum masaai", "Panicum mombasa", "Panicum tanzania", "Pennisetum purpureum", "Brachiaria mutica", 
+                                               "Brachiaria mulato I", "Panicum maximam", "Pennisetum pedicellatum", "Brachiaria decumbens Stapf")],
                 colorPalette = colors,
                 width = 60 * sqrt(ben_wards_data$total_beneficiaries) / sqrt(max(ben_wards_data$total_beneficiaries)), transitionTime = 0) %>%
   
@@ -83,3 +94,4 @@ map.ll <- leaflet() %>%
 
 
 saveWidget(map.ll, file = paste0(iDir, "/", "index", ".html", sep = ""))
+
